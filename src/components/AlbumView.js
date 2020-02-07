080ffe36a4861ed6bc4157c5612c5e5cb5749283
Joseph Tatum <josephtatum@gmail.com>
2020-02-06 17:22:09 -0800

    songs rendering on album view

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -4,8 +4,22 @@ import { Link } from 'react-router-dom';
 import useAlbum from '../hooks/useAlbum';
 
 const AlbumView = () => {
-  useAlbum();
-  return (<div></div>);
+  const { album } = useAlbum();
+
+  const mappedAlbum = album.map(({ id, title }) => {
+    return (
+      <li key={id}>
+        <Link to={`/song/${id}`}>
+          <h3>{title}</h3>
+        </Link>
+      </li>
+    );
+  });
+  return (
+    <ul>
+      {mappedAlbum}
+    </ul>
+  );
 };
 
 export default AlbumView;
